c8d81a84f24cae65278e679bb6349932c785dfc7
emontemayor <emmontemayor@pyrmaidacceptors.com>
2025-08-28 15:07:59 -0700

    clearing up warnings

--- conf.py
+++ conf.py
@@ -67,7 +67,7 @@ release = u'2.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -88,19 +88,6 @@ todo_include_todos = False
 #
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['build/_static']
-
 
 # -- Options for HTMLHelp output ------------------------------------------
 
